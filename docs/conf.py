8e2085c48ade99f85c877c4ba1373f9f1ec102b6
ljchang <lukejchang@gmail.com>
2015-06-04 07:40:33 -0600

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -110,7 +110,11 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+# on_rtd is whether we are on readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
