31fe4a80bacc7300152196f829f1c697cb2b0786
Sam Greydanus <samjgrey3@gmail.com>
2015-09-07 13:58:01 -0700

    another debugging
    I know this is not the right way to debug…but it’s the most convenient
    right now

--- nltools/searchlight.py
+++ nltools/searchlight.py
@@ -320,7 +320,7 @@ from nltools import Searchlight \n\
 import cPickle \n\
 import os \n\
 pdir = os.path.join(os.getcwd(),'searchlight.pickle') \n\
-params = cPickle.load(pdir) \n\
+params = cPickle.load(open(pdir)) \n\
 sl = Searchlight() \n\
 sl.predict(" + str(ith_core) + ", " + str(n_cores) + ", params) \n\
 exit 0" )
