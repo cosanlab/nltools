0f56407a30fa5dc338b44f8b8c016fc00074318d
Luke Chang <lukejchang@gmail.com>
2015-09-07 15:00:30 -0400

    clean up code - added new gray matter mask

--- nltools/__init__.py
+++ nltools/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ['analysis', 'plotting', 'stats','searchlight','__version__']
+__all__ = ['analysis', 'plotting', 'stats', 'cross_validation','utils','searchlight','__version__']
 
 from analysis import Predict, Roc, apply_mask
 from searchlight import Searchlight 
