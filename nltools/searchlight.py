21716bc7bcfc16994d1f06c19786976d2fb6df0a
Sam Greydanus <samjgrey3@gmail.com>
2015-09-07 15:02:34 -0700

    same as prev commit

--- nltools/searchlight.py
+++ nltools/searchlight.py
@@ -318,7 +318,7 @@ ipython  \n\
 from nltools.searchlight import Searchlight \n\
 import cPickle \n\
 import os \n\
-params = cPickle.load(open( os.path.join(os.getcwd(),'searchlight.pickle') )) \n\
+params = cPickle.load(open('searchlight.pickle') )) \n\
 sl = Searchlight() \n\
 sl.predict(" + str(ith_core) + ", " + str(n_cores) + ", params) \n\
 exit 0" )
