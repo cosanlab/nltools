2a1ca7b7ae71a54b2270caa52b7d87e215841dfd
Sam Greydanus <samjgrey3@gmail.com>
2015-09-11 09:34:07 -0700

    db

--- nltools/searchlight.py
+++ nltools/searchlight.py
@@ -124,7 +124,7 @@ import glob
 class Searchlight:
     def __init__(self, brain_mask=None, process_mask=None, radius=4, output_dir = None): #no scoring param
         self.resource_folder = os.path.join(os.getcwd(),'resources')
-        if save_output is None:
+        if output_dir is None:
             self.output_dir = os.path.join(os.getcwd(),'outfolder')
         else:
             self.output_dir = output_dir
