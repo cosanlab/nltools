997291393d5c4a8530bea57f2e9b0cf291e7e61b
Sam Greydanus <samjgrey3@gmail.com>
2015-09-12 18:58:40 -0700

    add simulator to init file

--- nltools/__init__.py
+++ nltools/__init__.py
@@ -2,5 +2,6 @@ __all__ = ['analysis', 'plotting', 'stats', 'cross_validation','utils','searchli
 
 from analysis import Predict, Roc, apply_mask
 from searchlight import Searchlight 
+from simulator import Simulator
 from version import __version__
 
