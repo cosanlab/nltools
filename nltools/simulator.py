3a91c4dc89cf3c1b977fb2ff76af0fed348d8b45
Sam Greydanus <samjgrey3@gmail.com>
2015-10-02 11:12:11 +0200

    Another simple bug fix

--- nltools/simulator.py
+++ nltools/simulator.py
@@ -143,7 +143,7 @@ class Simulator:
             #add noise and signal together, then convert to nifti files
             NF_list = []
             for i in xrange(len(I)):
-                NF_list.append(self.to_nifti(np.add(A_list[i],N_list[i]) ))
+                NF_list.append(self.to_nifti(np.add(N_list[i],N_list[i]) ))
                 
             if output_dir is not None:
                 if type(output_dir) is str:
