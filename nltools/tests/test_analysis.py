0757ab4484e2bf7f64e369f84b86a593b7a041e4
Luke Chang <lukejchang@gmail.com>
2015-10-09 07:51:11 -0400

    updated travisci tests

--- nltools/tests/test_analysis.py
+++ nltools/tests/test_analysis.py
@@ -3,23 +3,38 @@ import numpy as np
 import nibabel as nb
 from nilearn._utils import testing
 
-from nltools import analysis
+from nltools import analysis, simulator
 
+def test_simulator():
+    sim = simulator.Simulator()
+    r = 10
+    sigma = 1
+    y = [0,1]
+    n_reps = 10
+    sim.create_data(y, sigma, reps = n_reps, output_dir = None)
+    assert len(sim.data) == n_reps*len(y)
 
-def test_predict(tmpdir):
-    shape = (40, 41, 42)
-    length = 17
+def test_predict_svm(tmpdir):
+    sim = simulator.Simulator()
+    r = 10
+    sigma = .2
+    y = [0,1]
+    n_reps = 10
+    sim.create_data(y, sigma, reps = n_reps, output_dir = tmpdir)
 
-    img_dat, _ = testing.generate_fake_fmri(shape=shape, length=length)
-    Y = np.random.randint(2, size=length)
+    # shape = (40, 41, 42)
+    # length = 17
+
+    # img_dat, _ = testing.generate_fake_fmri(shape=shape, length=length)
+    # Y = np.random.randint(2, size=length)
 
     algorithm = 'svm'
     output_dir = str(tmpdir)
-    cv = {'type': 'kfolds', 'n_folds': 5}
+    cv = {'type': 'kfolds', 'n_folds': 5,'subject_id':sim.rep_id}
     extra = {'kernel': 'linear'}
     weightmap_name = "%s_weightmap.nii.gz" % algorithm
 
-    predict = analysis.Predict(img_dat, Y, algorithm=algorithm,
+    predict = analysis.Predict(sim.data, sim.y, algorithm=algorithm,
                                output_dir=output_dir,
                                cv_dict=cv,
                                **extra)
@@ -28,4 +43,32 @@ def test_predict(tmpdir):
 
     weightmap_img = nb.load(os.path.join(output_dir, weightmap_name))
 
-    assert weightmap_img.shape == shape
+    assert predict.mcr_xval==.99
+    assert weightmap_img.shape == sim.data[0].shape
+
+def test_predict_svr(tmpdir):
+    sim = simulator.Simulator()
+    r = 10
+    sigma = .1
+    y = [1,2,3]
+    n_reps = 10
+    sim.create_data(y, sigma, reps = n_reps, output_dir = tmpdir)
+
+    algorithm = 'svm'
+    output_dir = str(tmpdir)
+    cv = {'type': 'kfolds', 'n_folds': 5,'subject_id':sim.rep_id}
+    extra = {'kernel': 'linear'}
+    weightmap_name = "%s_weightmap.nii.gz" % algorithm
+
+    predict = analysis.Predict(sim.data, sim.y, algorithm=algorithm,
+                               output_dir=output_dir,
+                               cv_dict=cv,
+                               **extra)
+
+    predict.predict()
+
+    weightmap_img = nb.load(os.path.join(output_dir, weightmap_name))
+
+    assert predict.r_xval >= .99
+    assert weightmap_img.shape == sim.data[0].shape
+
