9ae4c9a7ba53741dacddf73fc1f4b5303fbe7750
Luke Chang <lukejchang@gmail.com>
2015-10-09 08:34:08 -0400

    updated travis tests

--- nltools/tests/test_analysis.py
+++ nltools/tests/test_analysis.py
@@ -10,10 +10,14 @@ def test_simulator(tmpdir):
     r = 10
     sigma = 1
     y = [0,1]
-    n_reps = 10
+    n_reps = 3
     output_dir = str(tmpdir)
     sim.create_data(y, sigma, reps = n_reps, output_dir = output_dir)
+
+    shape = (91, 109, 91)
+    sim_img = nb.load(os.path.join(output_dir, 'centered_sphere_0_0.nii.gz'))
     assert len(sim.data) == n_reps*len(y)
+    assert sim_img.shape == shape
 
 def test_predict_svm(tmpdir):
     sim = simulator.Simulator()
@@ -22,7 +26,7 @@ def test_predict_svm(tmpdir):
     y = [0,1]
     n_reps = 10
     output_dir = str(tmpdir)
-    sim.create_data(y, sigma, reps = n_reps, output_dir = output_dir)
+    sim.create_data(y, sigma, reps = n_reps, output_dir = None)
 
     # shape = (40, 41, 42)
     # length = 17
@@ -49,29 +53,29 @@ def test_predict_svm(tmpdir):
     assert weightmap_img.shape == sim.data[0].shape
 
 def test_predict_svr(tmpdir):
-    sim = simulator.Simulator()
-    r = 10
-    sigma = .1
-    y = [1,2,3]
-    n_reps = 10
-    output_dir = str(tmpdir)
-    sim.create_data(y, sigma, reps = n_reps, output_dir = output_dir)
+        sim = simulator.Simulator()
+        r = 10
+        sigma = .1
+        y = [1,2,3]
+        n_reps = 10
+        output_dir = str(tmpdir)
+        sim.create_data(y, sigma, reps = n_reps, output_dir = None)
 
-    algorithm = 'svm'
-    output_dir = str(tmpdir)
-    cv = {'type': 'kfolds', 'n_folds': 5,'subject_id':sim.rep_id}
-    extra = {'kernel': 'linear'}
-    weightmap_name = "%s_weightmap.nii.gz" % algorithm
+        algorithm = 'svr'
+        output_dir = str(tmpdir)
+        cv = {'type': 'kfolds', 'n_folds': 5,'subject_id':sim.rep_id}
+        extra = {'kernel': 'linear'}
+        weightmap_name = "%s_weightmap.nii.gz" % algorithm
 
-    predict = analysis.Predict(sim.data, sim.y, algorithm=algorithm,
-                               output_dir=output_dir,
-                               cv_dict=cv,
-                               **extra)
+        predict = analysis.Predict(sim.data, sim.y, algorithm=algorithm,
+                                   output_dir=output_dir,
+                                   cv_dict=cv,
+                                   **extra)
 
-    predict.predict()
+        predict.predict()
 
-    weightmap_img = nb.load(os.path.join(output_dir, weightmap_name))
+        weightmap_img = nb.load(os.path.join(output_dir, weightmap_name))
 
-    assert predict.r_xval >= .99
-    assert weightmap_img.shape == sim.data[0].shape
+        assert predict.r_xval >= .99
+        assert weightmap_img.shape == sim.data[0].shape
 
