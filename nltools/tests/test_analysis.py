75dfcfe8c6157b85255d0830fd1999b8a3c6d4a9
Luke Chang <lukejchang@gmail.com>
2015-10-09 09:22:36 -0400

    added roc test

--- nltools/tests/test_analysis.py
+++ nltools/tests/test_analysis.py
@@ -53,29 +53,58 @@ def test_predict_svm(tmpdir):
     assert weightmap_img.shape == sim.data[0].shape
 
 def test_predict_svr(tmpdir):
-        sim = simulator.Simulator()
-        r = 10
-        sigma = .1
-        y = [1,2,3]
-        n_reps = 10
-        output_dir = str(tmpdir)
-        sim.create_data(y, sigma, reps = n_reps, output_dir = None)
+    sim = simulator.Simulator()
+    r = 10
+    sigma = .1
+    y = [1,2,3]
+    n_reps = 10
+    output_dir = str(tmpdir)
+    sim.create_data(y, sigma, reps = n_reps, output_dir = None)
 
-        algorithm = 'svr'
-        output_dir = str(tmpdir)
-        cv = {'type': 'kfolds', 'n_folds': 5,'subject_id':sim.rep_id}
-        extra = {'kernel': 'linear'}
-        weightmap_name = "%s_weightmap.nii.gz" % algorithm
+    algorithm = 'svr'
+    output_dir = str(tmpdir)
+    cv = {'type': 'kfolds', 'n_folds': 5,'subject_id':sim.rep_id}
+    extra = {'kernel': 'linear'}
+    weightmap_name = "%s_weightmap.nii.gz" % algorithm
 
-        predict = analysis.Predict(sim.data, sim.y, algorithm=algorithm,
-                                   output_dir=output_dir,
-                                   cv_dict=cv,
-                                   **extra)
+    predict = analysis.Predict(sim.data, sim.y, algorithm=algorithm,
+                               output_dir=output_dir,
+                               cv_dict=cv,
+                               **extra)
 
-        predict.predict()
+    predict.predict()
 
-        weightmap_img = nb.load(os.path.join(output_dir, weightmap_name))
+    weightmap_img = nb.load(os.path.join(output_dir, weightmap_name))
 
-        assert predict.r_xval >= .99
-        assert weightmap_img.shape == sim.data[0].shape
+    assert predict.r_xval >= .99
+    assert weightmap_img.shape == sim.data[0].shape
+
+def test_roc(tmpdir):
+    sim = simulator.Simulator()
+    r = 10
+    sigma = .1
+    y = [0,1]
+    n_reps = 10
+    output_dir = str(tmpdir)
+    sim.create_data(y, sigma, reps = n_reps, output_dir = None)
+
+    algorithm = 'svm'
+    output_dir = str(tmpdir)
+    cv = {'type': 'kfolds', 'n_folds': 5,'subject_id':sim.rep_id}
+    extra = {'kernel': 'linear'}
+    weightmap_name = "%s_weightmap.nii.gz" % algorithm
+
+    predict = analysis.Predict(sim.data, sim.y, algorithm=algorithm,
+                               output_dir=output_dir,
+                               cv_dict=cv,
+                               **extra)
+
+    predict.predict()
+
+    # Single-Interval
+    roc = Roc(input_values=predict.yfit_xval, binary_outcome=np.array(sim.y)==1)
+    roc.plot()
+    roc.summary()
+    assert roc.accuracy==1
+
 
