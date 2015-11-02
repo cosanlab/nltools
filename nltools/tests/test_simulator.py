3526f69758240542ffb287decfeadeb2ae908f83
Luke Chang <lukejchang@gmail.com>
2015-11-01 23:15:09 -0500

    fixed tests

--- nltools/tests/test_simulator.py
+++ nltools/tests/test_simulator.py
@@ -1,21 +1,19 @@
 import os
 import numpy as np
 import nibabel as nb
-# from nilearn._utils import testing
-
-from nltools import analysis, simulator
-
+from nltools.simulator import Simulator
 
 def test_simulator(tmpdir):
-    sim = simulator.Simulator()
+    sim = Simulator()
     r = 10
     sigma = 1
     y = [0, 1]
     n_reps = 3
     output_dir = str(tmpdir)
     sim.create_data(y, sigma, reps=n_reps, output_dir=output_dir)
+    flist = glob.glob(os.path.join(tmpdir,'centered*nii.gz'))
 
     shape = (91, 109, 91)
-    sim_img = nb.load(os.path.join(output_dir, 'centered_sphere_0_0.nii.gz'))
+    sim_img = nb.concat_images(flist)
     assert len(sim.data) == n_reps*len(y)
-    assert sim_img.shape == shape
+    assert sim_img.shape[0:3] == shape
