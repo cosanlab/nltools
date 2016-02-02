a0f0fd0b0de4bb726fe0853f1d8f7b2cf5d730ef
Sam Greydanus <samjgrey3@gmail.com>
2016-02-02 11:13:19 -0500

    Another simple bug fix
    Former-commit-id: 43d58892fb18017f18bae13dc0c80173320929db

--- nltools/simulator.py
+++ nltools/simulator.py
@@ -290,7 +290,7 @@ class Simulator:
             if type(output_dir) is str:
                 if not os.path.isdir(output_dir):
                     os.makedirs(output_dir)
-                self.data.to_filename(os.path.join(output_dir,'centered_sphere_cor' + str(cor) + "_cov" + str(cov) + '_sigma' + str(sigma) + '.nii.gz'))
+                self.data.to_filename(os.path.join(output_dir,'maskdata_cor' + str(cor) + "_cov" + str(cov) + '_sigma' + str(sigma) + '.nii.gz'))
                 y_file = open(os.path.join(output_dir,'y.csv'), 'wb')
                 wr = csv.writer(y_file, quoting=csv.QUOTE_ALL)
                 wr.writerow(self.y)
