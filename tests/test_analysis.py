7733f5154ee0f9cfd4d7a522eef7944f99809378
Luke Chang <lukejchang@gmail.com>
2016-09-26 12:56:43 -0400

    updated tests

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -8,19 +8,19 @@ from nltools.data import Brain_Data
 import matplotlib
 matplotlib.use('TkAgg')
 
-def test_roc(tmpdir, sim):
-    sim = Simulator()
+def test_roc(tmpdir):
+    sim = simulator.Simulator()
 
     r = 10
     sigma = .1
     y = [0, 1]
     n_reps = 10
-    output_dir = str(tmpdir)
+    #     output_dir = str(tmpdir)
     sim.create_data(y, sigma, reps=n_reps, output_dir=None)
     dat = Brain_Data(data=sim.data,Y=pd.DataFrame(sim.y))
 
     algorithm = 'svm'
-    output_dir = str(tmpdir)
+    # output_dir = str(tmpdir)
     # cv = {'type': 'kfolds', 'n_folds': 5, 'subject_id': sim.rep_id}
     extra = {'kernel': 'linear'}
 
@@ -28,8 +28,12 @@ def test_roc(tmpdir, sim):
 
     # Single-Interval
     roc = analysis.Roc(input_values=output['yfit_all'], binary_outcome=output['Y'] == 1)
+    # roc = analysis.Roc(
+    #     input_values=predict.yfit_xval, binary_outcome=np.array(sim.y) == 1)
 
-    roc.plot()
+    roc.calculate()
+
+    # roc.plot()
 
     roc.summary()
     assert roc.accuracy == 1
