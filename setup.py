bdb06fdf103931865e72df9c43f72f230700f074
Luke Chang <lukejchang@gmail.com>
2016-12-26 06:48:53 -0500

    updated documentation
    Former-commit-id: 651c0cf7a45d1d8501f0b1ba7180e432c1cb87f2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # from nltools.version import __version__
 from setuptools import setup, find_packages
 
-__version__ = 0.2.5
+__version__ = '0.2.5'
 
 # try:
 #     from setuptools.core import setup
