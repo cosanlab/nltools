6c6e9685fb82b1e90eb7a17303079ae78871c60e
Luke Chang <lukejchang@gmail.com>
2016-12-26 19:23:35 -0500

    added multiple tutorials

--- examples/01_DataOperations/plot_mask.py
+++ examples/01_DataOperations/plot_mask.py
@@ -19,38 +19,65 @@ data = fetch_pain()
 # Apply_Mask
 # ---------------------------------------------------------
 #
-# We can calculate the pairwise spatial distance between all images in a Brain_Data()
-# instance using any method from sklearn or scipy.  This outputs an Adjacency() class
-# object.
+# Spherical masks can be created using the create_sphere function.
+# It requires specifying a center voxel and the radius of the sphere.
 
-s1 = create_sphere([41, 64, 55], radius=10)
-masked_dat = dat.apply_mask(s1)
-masked_dat[1].plot()
+from nltools.mask import create_sphere
+
+mask = create_sphere([41, 64, 55], radius=10)
+masked_data = data.apply_mask(mask)
+masked_data.mean().plot()
 
 #########################################################################
 # Extract Mean Within ROI
 # ---------------------------------------------------------
 #
-# We can calculate the pairwise spatial distance between all images in a Brain_Data()
-# instance using any method from sklearn or scipy.  This outputs an Adjacency() class
-# object.
+# We can easily calculate the mean within an ROI for each image within a 
+# Brain_Data() instance using the extract_roi() method. 
 
-mask = create_sphere([41, 64, 55], radius=10)
-out = dat.extract_roi(mask)
-plt.plot(out)
+mean = data.extract_roi(mask)
+plt.plot(mean)
 
 #########################################################################
 # Expand and Contract ROIs
 # ---------------------------------------------------------
 #
-# We can calculate the pairwise spatial distance between all images in a Brain_Data()
-# instance using any method from sklearn or scipy.  This outputs an Adjacency() class
-# object.
+# Some masks have many ROIs indicated by a unique ID.  It is possible to
+# expand these masks into separate ROIs and also collapse them into a single
+# image again.  Here we will demonstrate on a k=50 parcellation hosted on
+# http://neurovault.org.
+
+from nltools.mask import expand_mask, collapse_mask
+
+mask = Brain_Data('http://neurovault.org/media/images/2099/Neurosynth%20Parcellation_0.nii.gz')
+
+mask.plot()
+
+mask_x = expand_mask(mask)
+mask_x.plot()
+
+mask_c = collapse_mask(mask_x)
+mask_c.plot()
 
 #########################################################################
 # Threshold and Regions
 # ---------------------------------------------------------
 #
-# We can calculate the pairwise spatial distance between all images in a Brain_Data()
-# instance using any method from sklearn or scipy.  This outputs an Adjacency() class
-# object.
+# Images can be thresholded using an arbitrary cutoff or a percentile using the 
+# threshold method.  Here we calculate the mean of the high pain images and
+# threshold using the 95 percentile.
+
+high = data[np.where(data.X['PainLevel']==3)[0]]
+high.mean().threshold(threshold='95%').plot()
+
+#########################################################################
+# We might be interested in creating a binary mask from this threshold.
+
+mask = high.mean().threshold(threshold='95%',binarize=True)
+
+#########################################################################
+# We might also want to create separate images from each contiguous ROI.
+
+region = high.mean().threshold(threshold='95%').regions()
+region.plot()
+
