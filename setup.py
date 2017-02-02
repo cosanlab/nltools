c5bd558b36b55581bf7aa106ac3901afea4e954d
Luke Chang <lukejchang@gmail.com>
2017-02-02 10:29:31 -0500

    updated version to 0.2.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # from nltools.version import __version__
 from setuptools import setup, find_packages
 
-__version__ = '0.2.6'
+__version__ = '0.2.7'
 
 # try:
 #     from setuptools.core import setup
@@ -18,8 +18,9 @@ setup(
     author='Luke Chang',
     author_email='luke.j.chang@dartmouth.edu',
     url='http://neurolearn.readthedocs.org/en/latest/',
-    install_requires=['numpy', 'scipy', 'nilearn', 'nibabel','pandas', 'six', 'importlib',
-                      'seaborn', 'matplotlib', 'scikit-learn','pynv'],
+    install_requires=['numpy>=1.9', 'scipy', 'nilearn', 'nibabel>=2.0.1',
+                    'pandas>=0.16', 'six', 'importlib','seaborn', 'matplotlib', 
+                    'scikit-learn>=0.18.1','pynv'],
     packages=find_packages(exclude=['nltools/tests']),
     package_data={'nltools': ['resources/*']},
     license='LICENSE.txt',
