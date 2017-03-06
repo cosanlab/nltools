f83f2f74dbe33684a4876fc124464d9c376b2f23
ejolly <eshin.jolly@gmail.com>
2017-03-05 21:35:22 -0500

    Added zscore, downsample methods to Design_Mat class. Refactored code to prevent circular imports.
    Former-commit-id: 69b24382e99146ce794bb6330ea0077af68e3f4b

--- nltools/utils.py
+++ nltools/utils.py
@@ -7,21 +7,15 @@ handy utilities.
 '''
 __all__ = ['get_resource_path',
             'get_anatomical',
-            'set_algorithm',
-            'onsets_to_dm']
+            'set_algorithm']
 __author__ = ["Luke Chang"]
 __license__ = "MIT"
 
 from os.path import dirname, join, pardir, sep as pathsep
-import pandas as pd
-import numpy as np
 import nibabel as nib
 import importlib
 import os
 from sklearn.pipeline import Pipeline
-import six
-#from nltools.data import Design_Mat
-import warnings
 
 def get_resource_path():
     """ Get path to nltools resource directory. """
@@ -105,64 +99,3 @@ def set_algorithm(algorithm, **kwargs):
             'randomforestClassifier'.""")
 
     return predictor_settings
-
-def onsets_to_dm(filePath,TR,runLength,header='infer',sort=False,addIntercept=True,**kwargs):
-    """
-        Function read in a 2 or 3 column onsets file, specified in seconds, organized as:
-        'Stimulus,Onset','Onset,Stimulus','Stimulus,Onset,Duration', or 'Onset,Duration,Stimulus'.
-        Args:
-            filePath: (str) path to file
-            TR: (float) length of TR in seconds the run was collected at 
-            runLength: (int) number of TRs in the run these onsets came from
-            sort: (bool) whether to sort the columns of the resulting design matrix alphabetically
-            addIntercept: (bool) whether to add an intercept to the resulting dataframe
-            header: (str) None if missing header, otherwise pandas header keyword
-            kwargs: additional inputs to pandas.read_csv
-        Output:
-            Design_Matrix class
-    """
-
-    df = pd.read_csv(filePath,header=header,**kwargs)
-    if df.shape[1] == 2:
-        warnings.warn("Only 2 columns in file, assuming all stimuli are the same duration")
-    elif df.shape[1] == 1 or df.shape[1] > 3:
-        raise ValueError("Can only handle files with 2 or 3 columns!")
-
-    #Try to infer the header
-    if header is None:
-        possibleHeaders = ['Stimulus','Duration','Onset']
-        if isinstance(df.iloc[0,0],six.string_types):
-            df.columns = possibleHeaders[:df.shape[1]]
-        elif isinstance(df.iloc[0,df.shape[1]-1],six.string_types):
-            df.columns = possibleHeaders[1:] + [possibleHeaders[0]]
-        else:
-            raise ValueError("Can't figure out data organization. Make sure file has no more than 3 columns specified as 'Stimulus,Onset,Duration' or 'Onset,Duration,Stimulus'")
-    df['Onset'] = df['Onset'].apply(lambda x: int(np.floor(x/TR)))
-
-    #Build dummy codes
-    X = Design_Mat(columns=df['Stimulus'].unique(),data=np.zeros([runLength,len(df['Stimulus'].unique())]))
-    for i, row in df.iterrows():
-        if df.shape[1] == 3:
-            dur = np.ceil(row['Duration']/TR)
-            X.ix[row['Onset']-1:row['Onset']+dur-1,row['Stimulus']]=1
-        elif df.shape[1] == 2:
-            X.ix[row['Onset'],row['Stimulus']] = 1
-
-    if sort:
-        X = X.reindex_axis(sorted(X.columns),axis=1)
-
-    if addIntercept:
-        X['intercept'] = 1
-
-    return X
-
-
-
-
-
-
-
-
-
-
-
