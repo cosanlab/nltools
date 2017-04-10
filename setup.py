c734fdc5e31bc48ad2b473060acdec9c5445d5db
Luke Chang <lukejchang@gmail.com>
2017-04-09 22:47:05 -0400

    updated version to 0.2.8
    Former-commit-id: b0eda1e14acf9b624273c44333d61d8891bb1b91

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # from nltools.version import __version__
 from setuptools import setup, find_packages
 
-__version__ = '0.2.7'
+__version__ = '0.2.8'
 
 # try:
 #     from setuptools.core import setup
@@ -19,7 +19,7 @@ setup(
     author_email='luke.j.chang@dartmouth.edu',
     url='http://neurolearn.readthedocs.org/en/latest/',
     install_requires=['numpy>=1.9', 'scipy', 'nilearn', 'nibabel>=2.0.1',
-                    'pandas>=0.16', 'six', 'importlib','seaborn', 'matplotlib', 
+                    'pandas>=0.16', 'six', 'importlib','seaborn', 'matplotlib',
                     'scikit-learn>=0.18.1','pynv'],
     extras_require={
     'ibrainViewer':['ipywidgets>=5.2.2']
@@ -28,7 +28,10 @@ setup(
     package_data={'nltools': ['resources/*']},
     license='LICENSE.txt',
     description='A Python package to analyze neuroimaging data',
-    long_description='nltools is a collection of python tools to perform preprocessing, univariate GLMs, and predictive multivariate modeling of neuroimaging data. It is the analysis engine powering www.neuro-learn.org.',
+    long_description='nltools is a collection of python tools to perform '
+                     'preprocessing, univariate GLMs, and predictive '
+                     'multivariate modeling of neuroimaging data. It is the '
+                     'analysis engine powering www.neuro-learn.org.',
     keywords = ['neuroimaging', 'preprocessing', 'analysis','machine-learning'],
     classifiers = [
         "Programming Language :: Python",
@@ -38,4 +41,3 @@ setup(
     ],
     **extra_setuptools_args
 )
-
