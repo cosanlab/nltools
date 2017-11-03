4478994460c7cff7450401f2277312d24bf48aa7
Luke Chang <lukejchang@gmail.com>
2017-11-03 15:48:00 -0700

    Fixed bug with create_mask in python3

---
+++ nltools/tests/test_mask.py
@@ -0,0 +1,11 @@
+from nltools.mask import create_sphere
+
+def test_create_sphere():
+	a = create_sphere(radius=10, coordinates=[0, 0, 0])
+	assert a.sum() == 515
+	a = create_sphere(radius=[10,5], coordinates=[[0, 0, 0],
+						[15, 0, 25]])
+	assert a.sum() == 571
+	a = create_sphere(radius=10, coordinates=[[0, 0, 0],
+						[15, 0, 25]])
+	assert a.sum() == 1051
