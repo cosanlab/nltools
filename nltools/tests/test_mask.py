e6c50319bcd10c31ab43e01ddda6a8ac2896ddee
ejolly <eshin.jolly@gmail.com>
2017-11-06 05:33:28 -0500

    Updated sphere test to account for different number of masking voxels.
    Former-commit-id: a248af13d9c940d6fd16fd0af2270b216a059bcf

--- nltools/tests/test_mask.py
+++ nltools/tests/test_mask.py
@@ -2,11 +2,12 @@ from nltools.mask import create_sphere
 import numpy as np
 
 def test_create_sphere():
+	# Test values update to reflect the fact that standard Brain_Data mask has few voxels because ventricles are 0'd out
 	a = create_sphere(radius=10, coordinates=[0, 0, 0])
-	assert np.sum(a.get_data()) >= 515
+	assert np.sum(a.get_data()) >= 497 #515
 	a = create_sphere(radius=[10, 5], coordinates=[[0, 0, 0],
 	                    [15, 0, 25]])
-	assert np.sum(a.get_data()) >= 571
+	assert np.sum(a.get_data()) >= 553 #571
 	a = create_sphere(radius=10, coordinates=[[0, 0, 0],
 	                    [15, 0, 25]])
-	assert np.sum(a.get_data()) >= 1051
+	assert np.sum(a.get_data()) >= 1013 #1051
