efe6db1be396208d9425f1463cf4f4431890dd0c
Luke Chang <lukejchang@gmail.com>
2017-11-25 23:35:29 -0500

    updated setup
    Former-commit-id: bf110d72fba975086a43a6cdf5a15694dd04348d

--- setup.py
+++ setup.py
@@ -1,30 +1,32 @@
-# from nltools.version import __version__
 from setuptools import setup, find_packages
 
-__version__ = '0.3.4'
+version = {}
+with open("nltools/version.py") as f:
+    exec(f.read(), version)
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 extra_setuptools_args = dict(
     tests_require=['pytest']
 )
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 setup(
-    name='nltools',
-    version=__version__,
-    author='Luke Chang',
-    author_email='luke.j.chang@dartmouth.edu',
-    url='http://neurolearn.readthedocs.org/en/latest/',
-    install_requires=requirements,
-    extras_require={
+    name = 'nltools',
+    version = version['__version__'],
+    author = 'Luke Chang',
+    author_email = 'luke.j.chang@dartmouth.edu',
+    url = 'http://neurolearn.readthedocs.org/en/latest/',
+    install_requires = requirements,
+    extras_require = {
     'ibrainViewer':['ipywidgets>=5.2.2']
     },
-    packages=find_packages(exclude=['nltools/tests']),
-    package_data={'nltools': ['resources/*']},
-    license='LICENSE.txt',
-    description='A Python package to analyze neuroimaging data',
-    long_description='nltools is a collection of python tools to perform '
+    packages = find_packages(exclude=['nltools/tests']),
+    package_data = {'nltools': ['resources/*']},
+    include_package_data = True,
+    license = 'LICENSE.txt',
+    description = 'A Python package to analyze neuroimaging data',
+    long_description = 'nltools is a collection of python tools to perform '
                      'preprocessing, univariate GLMs, and predictive '
                      'multivariate modeling of neuroimaging data. It is the '
                      'analysis engine powering www.neuro-learn.org.',
