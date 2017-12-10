91156f8dafa8fffc6eecf35c40905fdbbb22da90
Luke Chang <lukejchang@gmail.com>
2017-12-10 17:21:55 -0500

    updated relative imports
    Former-commit-id: 30d4d0ce5b41e73687650ef17695ca431fa325ce

---
+++ nltools/external/__init__.py
@@ -0,0 +1,10 @@
+"""
+External functions
+"""
+
+from .srm import DetSRM, SRM
+from .hrf import (spm_hrf,
+                    glover_hrf,
+                    spm_time_derivative,
+                    glover_time_derivative,
+                    spm_dispersion_derivative)
