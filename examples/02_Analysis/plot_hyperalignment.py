59ba770257c2d984034b1779eb7d4a3868379010
Luke Chang <lukejchang@gmail.com>
2017-12-13 00:14:34 -0500

    Updated functional alignment tutorial

--- examples/02_Analysis/plot_hyperalignment.py
+++ examples/02_Analysis/plot_hyperalignment.py
@@ -11,18 +11,17 @@ target voxel by averaging activity in surrounding voxels with a gaussian kernel.
 However, these techniques are limited when learning multivariate models, where
 voxel alignment across participants is critical to making accurate inference.
 There have been several developments in improving 'functional alignment'.
-Jim Haxby's group has pioneered `'hyperalignment'
-<https://academic.oup.com/cercor/article/26/6/2919/1754308>`, which uses an
+Jim Haxby's group has pioneered `hyperalignment
+<https://academic.oup.com/cercor/article/26/6/2919/1754308>`_, which uses an
 iterative procrustes transform to scale, rotate, and reflect voxel time series
 so that they are in the same functional space across participants. They have
 found that this technique can dramatically improve between subject
 classification accuracy particularly in `ventral temporal cortex
-<http://haxbylab.dartmouth.edu/publications/HGC+11.pdf>`. This technique is
-implemented in the `PyMVPA Toolsbox <http://www.pymvpa.org/>`. Another promising
-functional alignment technique known as `Shared Response Model
-<http://papers.nips.cc/paper/5855-a-reduced-dimension-fmri-shared-response-model.pdf>`
- was developed at Princeton to improve intersubject-connectivity analyses and is
- implemented in the `brainiak toolbox <https://github.com/brainiak/brainiak>`.
+<http://haxbylab.dartmouth.edu/publications/HGC+11.pdf>`_. This technique is
+implemented in the `PyMVPA <http://www.pymvpa.org/>`_ toolbox. Another promising
+functional alignment technique known as the `Shared Response Model <http://papers.nips.cc/paper/5855-a-reduced-dimension-fmri-shared-response-model.pdf>`_
+was developed at Princeton to improve intersubject-connectivity analyses and is
+implemented in the `brainiak <https://github.com/brainiak/brainiak>`_ toolbox.
 They also have found that this technique can improve between subject analyses.
 This method has several additional interesting properties such as the ability
 to learn a lower dimensional common representational space and also a
@@ -34,7 +33,7 @@ using nltools.
 
 #########################################################################
 # Simulate Data
-# ---------
+# -------------
 #
 # First, let's simulate some data to align. Here we will simulate 3 subjects
 # with 100 data points. Each subject has signal in 30% of the voxels in the
@@ -81,7 +80,7 @@ def simulate_data(n_observations, y, p, sigma, mask):
 mask = create_sphere([0, 45, 0], radius=8)
 data = [simulate_data(n_observations, y, p, sigma, mask) for x in range(n_sub)]
 
-plt.figure(figsize=(15,3))
+plt.figure(figsize=(10,3))
 plt.plot(y)
 plt.title('Simulated Signal', fontsize=20)
 plt.xlabel('Time', fontsize=18)
@@ -123,7 +122,7 @@ f,a = plt.subplots(nrows=2, ncols=3, figsize=(15,5), sharex=True, sharey=True)
 [a[0,i].imshow(x.data.T, aspect='auto') for i,x in enumerate(data)]
 [a[1,i].imshow(x.data.T, aspect='auto') for i,x in enumerate(out['transformed'])]
 a[0,0].set_ylabel('Original Voxels', fontsize=16)
-a[1,0].set_ylabel('Aligned Voxels', fontsize=16)
+a[1,0].set_ylabel('Aligned Features', fontsize=16)
 [a[1,x].set_xlabel('Time', fontsize=16) for x in range(3)]
 [a[0,x].set_title('Subject %s' % str(x+1), fontsize=16) for x in range(3)]
 plt.tight_layout()
@@ -142,17 +141,20 @@ a[1].set_title('Average Voxel x Time Matrix of Aligned Data', fontsize=16)
 # -------------------------------------------------------
 #
 # The transformation matrices can be used to project each subject's aligned
-# data into the original subject specific voxel space.
+# data into the original subject specific voxel space. The procrustes method
+# doesn't look identical as there are a few processing steps that occur within
+# the algorithm that would need to be accounted for to fully recover the original
+# data (e.g., centering, and scaling by norm).
 
-original_data = [np.dot(t.data,tm.T) for t,tm in zip(out['transformed'],out['transformation_matrix'])]
+original_data = [np.dot(t.data, tm.T) for t,tm in zip(out['transformed'], out['transformation_matrix'])]
 
 f,a = plt.subplots(nrows=3, ncols=3, figsize=(15,10), sharex=True, sharey=True)
-[a[0,i].imshow(x.data.T, aspect='auto') for i,x in enumerate(data)]
-[a[1,i].imshow(x.data.T, aspect='auto') for i,x in enumerate(out['transformed'])]
-[a[2,i].imshow(x.T, aspect='auto') for i,x in enumerate(original_data)]
-[a[i,0].set_ylabel(x,fontsize=16) for i,x in enumerate(['Original Voxels','Aligned Voxels', 'Backprojected Voxels'])]
-[a[2,x].set_xlabel('Time', fontsize=16) for x in range(3)]
-[a[0,x].set_title('Subject %s' % str(x+1), fontsize=16) for x in range(3)]
+[a[0, i].imshow(x.data.T, aspect='auto') for i, x in enumerate(data)]
+[a[1, i].imshow(x.data.T, aspect='auto') for i, x in enumerate(out['transformed'])]
+[a[2, i].imshow(x.T, aspect='auto') for i, x in enumerate(original_data)]
+[a[i, 0].set_ylabel(x,fontsize=16) for i, x in enumerate(['Original Voxels','Aligned Features', 'Backprojected Voxels'])]
+[a[2, x].set_xlabel('Time', fontsize=16) for x in range(3)]
+[a[0, x].set_title('Subject %s' % str(x+1), fontsize=16) for x in range(3)]
 plt.tight_layout()
 
 
@@ -166,14 +168,40 @@ plt.tight_layout()
 # original subject voxel space.
 
 d3 = data[2]
-d3_out = d3.align(out['common_model'],method='deterministic_srm')
-bp = np.dot(d3_out['transformed'].data,d3_out['transformation_matrix'].T)
+d3_out = d3.align(out['common_model'], method='deterministic_srm')
+bp = np.dot(d3_out['transformed'].data, d3_out['transformation_matrix'].T)
 
 f,a = plt.subplots(ncols=3, figsize=(15,5), sharex=True, sharey=True)
 a[0].imshow(d3.data.T, aspect='auto')
 a[1].imshow(d3_out['transformed'].data.T, aspect='auto')
 a[2].imshow(bp.T, aspect='auto')
-[a[i].set_title(x,fontsize=18) for i,x in enumerate(['Original Data','Transformed_Data','Backprojected Data'])]
+[a[i].set_title(x,fontsize=18) for i, x in enumerate(['Original Data',' Transformed_Data', 'Backprojected Data'])]
 [a[x].set_xlabel('Time', fontsize=16) for x in range(2)]
 a[0].set_ylabel('Voxels', fontsize=16)
+plt.tight_layout()
+
+#########################################################################
+# Align subjects in lower dimensional common space
+# ---------------------------------
+#
+# The shared response model allows for the possibility of aligning in a lower
+# dimensional functional space. Here we provide an example of aligning to a 10
+# dimensional features space. Previous work has found that this can potentially
+# improve generalizability of multivariate models trained on an ROI compared to
+# using as many features as voxels. This feature is not yet implemented for
+# procrustes transformation as dimensionality reduction would need to happen
+# either before or after alignment.
+
+n_features = 10
+out = align(data, method='probabilistic_srm', n_features=n_features)
+
+original_data = [np.dot(t.data,tm.T) for t,tm in zip(out['transformed'],out['transformation_matrix'])]
+
+f,a = plt.subplots(nrows=3, ncols=3, figsize=(15,10), sharex=True, sharey=False)
+[a[0, i].imshow(x.data.T, aspect='auto') for i, x in enumerate(data)]
+[a[1, i].imshow(x.data.T, aspect='auto') for i, x in enumerate(out['transformed'])]
+[a[2, i].imshow(x.T, aspect='auto') for i, x in enumerate(original_data)]
+[a[i, 0].set_ylabel(x, fontsize=16) for i, x in enumerate(['Original Voxels','Aligned Features', 'Backprojected Voxels'])]
+[a[2, x].set_xlabel('Time', fontsize=16) for x in range(3)]
+[a[0, x].set_title('Subject %s' % str(x+1), fontsize=16) for x in range(3)]
 plt.tight_layout()
