68275421dadbd007fed6ae53cc188b31981887aa
ejolly <eshin.jolly@gmail.com>
2018-09-13 19:32:20 -0400

    Added stats functions for distance correlation test and associated matrix centering function as well as procrustes similarity test.
    Former-commit-id: 2932de44e3b8ba5b0fd25a5cf6af0ae5b35ad9b9

--- nltools/stats.py
+++ nltools/stats.py
@@ -36,7 +36,7 @@ import numpy as np
 import pandas as pd
 from scipy.stats import pearsonr, spearmanr, kendalltau, norm
 from scipy.stats import t as t_dist
-from scipy.spatial.distance import squareform
+from scipy.spatial.distance import squareform, pdist
 from copy import deepcopy
 import nibabel as nib
 from scipy.interpolate import interp1d
@@ -47,6 +47,7 @@ import six
 from .utils import attempt_to_import, check_square_numpy_matrix
 from .external.srm import SRM, DetSRM
 from scipy.linalg import orthogonal_procrustes
+from scipy.spatial import procrustes
 from sklearn.utils import check_random_state
 
 MAX_INT = np.iinfo(np.int32).max
@@ -1191,3 +1192,175 @@ def procrustes(data1, data2):
     disparity = np.sum(np.square(mtx1 - mtx2))
 
     return mtx1, mtx2, disparity, R, s
+
+def double_center(mat):
+    '''Double center a 2d array.
+    
+    Args:
+        mat (ndarray): 2d numpy array
+    
+    Returns:
+        mat (ndarray): double-centered version of input
+
+    '''
+
+    if len(mat.shape) != 2:
+        raise ValueError('Array should be 2d')
+
+    # keepdims ensures that row/column means are not incorrectly broadcast during    
+    subtraction
+    row_mean = mat.mean(axis=0,keepdims=True)
+    col_mean = mat.mean(axis=1,keepdims=True)
+    grand_mean = mat.mean()
+    return mat - row_mean - col_mean + grand_mean  
+
+def u_center(mat):
+    '''U-center a 2d array. U-centering is a bias-corrected form of double-centering
+    
+    Args:
+        mat (ndarray): 2d numpy array
+        
+    Returns:
+        mat (narray): u-centered version of input
+    '''
+
+    if len(mat.shape) != 2:
+        raise ValueError('Array should be 2d')
+    
+    dim = mat.shape[0]
+    u_mu = mat.sum() / ((dim - 1) * (dim - 2))
+    sum_cols = mat.sum(axis=0,keepdims=True)
+    sum_rows = mat.sum(axis=1, keepdims=True)
+    u_mu_cols = np.ones((dim, 1)).dot(sum_cols / (dim - 2))
+    u_mu_rows = (sum_rows / (dim - 2)).dot(np.ones((1, dim)))  
+    out = np.copy(mat)
+    # Do one operation at a time, to improve broadcasting memory usage.
+    out -= u_mu_rows
+    out -= u_mu_cols
+    out += u_mu
+    # The diagonal is zero
+    out[np.eye(dim, dtype=bool)] = 0
+
+def distance_correlation(x,y,bias_corrected=True,return_covs=False):
+    '''Compute the distance correlation betwen 2 arrays. 
+        Distance correlation involves computing the normalized covariance of two centered euclidean distance matrices. Each distance matrix is the euclidean distance between rows (if x or y are 2d) or scalars (if x or y are 1d). Each matrix is centered using u-centering, a bias-corrected form of double-centering. This permits inference of the normalized covariance between each distance matrix using a t-test.  (Szekely & Rizzo, 2013).    
+        
+    Args:
+        x (ndarray): 1d or 2d numpy array of observations by features
+        y (ndarry): 1d or 2d numpy array of observations by features
+        bias_corrected (bool): if false use double-centering but no inference test is performed, if true use u-centering and perform inference; default True
+        return_covs (bool): if true return distance covariance and variances of each array as well; default False
+    
+    Returns:
+        results (dict): dictionary of results (correlation, t, p, and df.) Optionally, covariance, x variance, and y variance
+    '''
+    
+    if len(x.shape) > 2 or len(y.shape) > 2:
+        raise ValueError("Both arrays must be 1d or 2d")
+    
+    # 1 compute euclidean distances between pairs of value in each array
+    if len(x.shape) == 1:
+        _x = x[:,np.newaxis]
+    else:
+        _x = x
+    if len(y.shape) == 1:
+        _y = y[:,np.newaxis]
+    else:
+        _y = y
+
+    x_dist = squareform(pdist(_x))
+    y_dist = squareform(pdist(_y))
+    
+    # 2 center each matrix
+    if bias_corrected:
+        # U-centering
+        x_dist_cent = u_center(x_dist)
+        y_dist_cent = u_center(y_dist)
+        # Compute covariances using N*(N-3) in denominator
+        adjusted_n = _x.shape[0] * (_x.shape[0]-3)
+        xy = np.multiply(x_dist_cent,y_dist_cent).sum() / adjusted_n
+        xx = np.multiply(x_dist_cent,x_dist_cent).sum() / adjusted_n
+        yy = np.multiply(y_dist_cent,y_dist_cent).sum() / adjusted_n
+    else:
+        # double-centering
+        x_dist_cent = double_center(x_dist)
+        y_dist_cent = double_center(y_dist)
+        # Compute covariances using N^2 in denominator
+        xy = np.multiply(x_dist_cent,y_dist_cent).mean()
+        xx = np.multiply(x_dist_cent,x_dist_cent).mean()
+        yy = np.multiply(y_dist_cent,y_dist_cent).mean()
+    
+    # 3 compute covariances and variances  
+    cov = np.sqrt(xy)
+    var_x = np.sqrt(xx)
+    var_y = np.sqrt(yy)
+
+    # 4 Normalize to get correlation
+    denom = np.sqrt(xx * yy)
+    if denom > 0:
+        r2 = xy / denom
+    else:
+        r2 = 0
+    cor = np.sqrt(np.abs(r2)) # because u-centering can produce negative dcors
+    
+    out = {}
+    out['d_correlation'] = cor
+
+    if return_covs:
+        out['d_covariance'] = cov
+        out['x_var'] = var_x
+        out['y_var'] = var_y
+
+    if bias_corrected:
+        dof = (adjusted_n / 2) - 1
+        t = np.sqrt(dof) * (r2 / np.sqrt(1 - r2**2))
+        p = 2*(1-t_dist.cdf(np.abs(t), dof))
+        out['t'] = t
+        out['p'] = p
+        out['df'] = dof
+
+    return out
+
+def procrustes_similarity(mat1,mat2,n_permute=5000,tail=2,n_jobs=-1,random_state=None):
+    """ Use procrustes super-position to perform a similarity test between 2 matrices. Matrices need to match in size on their first dimension only, as the smaller matrix on the second dimension will be padded with zeros. After aligning two matrices using the procrustes transformation, use the computed disparity between them (sum of squared error of elements) as a similarity metric. Shuffle the rows of one of the matrices and recompute the disparity to perform inference (Peres-Neto & Jackson, 2001). Note: by default this function reverses disparity to treat it like a *similarity* measure like correlation, rather than a distance measure like correlation distance, i.e. smaller values mean less similar, larger values mean more similar.
+    
+    Args:
+        mat1 (ndarray): 2d numpy array; must have same number of rows as mat2
+        mat2 (ndarray): 1d or 2d numpy array; must have same number of rows as mat1
+        n_permute (int): number of permutation iterations to perform
+        tail (int): either 1 for one-tailed or 2 for two-tailed test; default 2
+        n_jobs (int): The number of CPUs to use to do permutation; default -1 (all)
+    
+    Returns:
+        similarity (float): similarity between matrices bounded between 0 and 1
+        pval (float): permuted p-value
+
+    """
+   
+    if mat1.shape[0] != mat2.shape[0]:
+        raise ValueError('Both arrays must match on their first dimension')
+    
+    random_state = check_random_state(random_state)
+    
+    # Make sure both matrices are 2d and the same dimension via padding
+    if len(mat1.shape) < 2:
+        mat1 = mat1[:,np.newaxis]
+    if len(mat2.shape) < 2:
+        mat2 = mat2[:,np.newaxis]
+    if mat1.shape[1] > mat2.shape[1]:
+        mat2 = np.pad(mat2,((0,0),(0, mat1.shape[1] - mat2.shape[1])),'constant')
+    elif mat2.shape[1] > mat1.shape[1]:
+        mat1 = np.pad(mat1,((0,0),(0, mat2.shape[1] - mat1.shape[1])),'constant')
+    
+     _,_,sse = procrustes(mat1,mat2)
+    sse = 1 - sse # flip to similarity measure
+    
+    stats = dict()
+    stats['similarity'] = sse
+    
+    all_p = Parallel(n_jobs=n_jobs)(delayed(procrustes)(random_state.permutation(mat1),mat2) for i in range(n_permute))
+    all_p = [1 - x[2] for x in all_p]
+    
+    stats['p'] = _calc_pvalue(all_p,sse,tail)
+                                    
+    return stats
