07ba22dff4ba3baf858ff37ed50b869200007a3e
ejolly <eshin.jolly@gmail.com>
2019-06-01 15:25:37 -0400

    Fixed bug in which plotting used MNI_Template preference setting rather than resolution of current Brain_Data object. Also fixed bug where iplot would fail to render background image.

--- nltools/stats.py
+++ nltools/stats.py
@@ -1264,7 +1264,6 @@ def procrustes(data1, data2):
             dot(R.T, R) == I.
         scale : float
             Sum of the singular values of ``dot(data1.T, data2)``.
-
     '''
 
     mtx1 = np.array(data1, dtype=np.double, copy=True)
