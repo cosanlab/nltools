Quick fix for plotting isometric voxels where the affine involves a rotation.

nltools/utils.py
@@ -79,7 +79,7 @@ def get_mni_from_img_resolution(brain, img_type='plot'):
     if img_type not in ['plot', 'brain']:
         raise ValueError("img_type must be 'plot' or 'brain' ")
     
-    res_array = np.diag(brain.nifti_masker.affine_)[:3]
+    res_array = np.abs(np.diag(brain.nifti_masker.affine_)[:3])
     voxel_dims = np.unique(res_array)
     if len(voxel_dims) != 1:
         raise ValueError("Voxels are not isometric and cannot be visualized in standard space")