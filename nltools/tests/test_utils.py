7f010e7c1f0d86677ee0d6029d7a8ad710df2d7b
ljchang <luke.j.chang@dartmouth.edu>
2020-04-06 14:29:05 -0400

    added tests for check_brain utils

--- nltools/tests/test_utils.py
+++ nltools/tests/test_utils.py
@@ -1,4 +1,15 @@
 
+from nltools.utils import check_brain_data, check_brain_data_is_single
+from nltools.mask import create_sphere
+import numpy as np
+
+def check_brain_data(sim_brain_data):
+    mask = Brain_Data(create_sphere([15, 10, -8], radius=10))
+    a = check_brain_data(sim_brain_data)
+    assert isinstance(a, Brain_Data)
+    b = check_brain_data(sim_brain_data, mask=mask)
+    assert isinstance(b, Brain_Data)
+    assert b.shape()[1] == np.sum(mask.data==1)
 
 def check_brain_data_is_single(sim_brain_data):
     assert not check_brain_data_is_single(sim_dat)
