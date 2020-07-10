d93c640a24c5ac5bdc546d039f13a98a62649a4c
Luke Chang <luke.j.chang@dartmouth.edu>
2020-07-10 09:37:01 -0400

    updated RTD

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ version = version['__version__']
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+#needs_sphinx = '3.2.1'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
