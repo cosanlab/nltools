9af51e7d447c51f8f331090ef6a1a09113d0baf8
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-10-03 20:54:57 -0400

    pin python>=3.6 in install_requires and update PyPI classifiers

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
     author = 'Cosan Lab',
     author_email = 'luke.j.chang@dartmouth.edu',
     url = 'http://neurolearn.readthedocs.org/en/latest/',
+    python_requires = '>=3.6',
     install_requires = requirements,
     extras_require = {
     'interactive_plots':['ipywidgets>=5.2.2']
@@ -32,8 +33,9 @@ setup(
                      'analysis engine powering www.neuro-learn.org.',
     keywords = ['neuroimaging', 'preprocessing', 'analysis','machine-learning'],
     classifiers = [
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License"
